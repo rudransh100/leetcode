a61cecba0493510e6870ad08ad05002d6f9b4ef8
RUDRANSH <rudranshupadhyay2004@gmail.com>
2026-03-20 13:18:35 +0530

    Time: 1 ms (78.46%), Space: 43.1 MB (45.84%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,21 @@
+class Solution {
+    String normalizeString(String str){
+        StringBuilder sb = new StringBuilder();
+        for(char c : str.toCharArray()){
+            if(c!='#'){
+                sb.append(c);
+            }
+            else{
+                if(!sb.isEmpty()){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+
+        }
+        return sb.toString();
+
+    }
+    public boolean backspaceCompare(String s, String t) {
+        return normalizeString(s).equals(normalizeString(t));
+    }
+}
