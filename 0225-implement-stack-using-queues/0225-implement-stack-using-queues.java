402c438cfd358a3e056649302aab66089c9f702e
RUDRANSH <rudranshupadhyay2004@gmail.com>
2026-07-09 21:03:28 +0530

    Time: 1 ms (50.72%), Space: 43.1 MB (11.32%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -3,13 +3,13 @@ class MyStack {
     public MyStack() {
         q = new LinkedList<>();
     }
-    
     public void push(int x) {
         q.add(x);
         for(int i=0;i<q.size()-1;i++){
             q.add(q.remove());
         }
     }
+    
     
     public int pop() {
         return q.remove();
