c38a4b50f19d2db8c94261efd315d218d3cbd565
RUDRANSH <rudranshupadhyay2004@gmail.com>
2026-07-21 01:55:40 +0530

    Time: 5 ms (83.72%), Space: 47 MB (92.2%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,43 +1,37 @@
 class Solution {
-    void shift(int[][] grid, int row, int col) {
-        int temp = 0;
-        for (int i = row-1; i >= 0; i--) {
-            for (int j = col-1; j >= 0; j--) {
-                if (j == 0) {
-                    temp = j;
-                    continue;
-                }
-                temp = grid[i][j];
-                grid[i][j] = grid[i ][j-1];
-                grid[i][j-1] = temp;
-            }
-            if(i!=0){
-                int x = grid[i][temp];
-                grid[i][temp]= grid[i-1][col-1];
-                grid[i-1][col-1] = x;
-            }
-        }
-        return;
-    }
-
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        List<List<Integer>> matrix = new ArrayList<>();
+
         int m = grid.length;
         int n = grid[0].length;
-        k = k%(m*n);
-        for (int i = 0; i < k; i++) {
-            shift(grid, m, n);
-        }
+        int total = m * n;
+
+        k %= total;
+
+        int[][] ans = new int[m][n];
 
         for (int i = 0; i < m; i++) {
-            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
 
-            for (int j = 0; j < n;j++) {
-                row.add(grid[i][j]);
+                int oldIndex = i * n + j;
+                int newIndex = (oldIndex + k) % total;
+
+                int newRow = newIndex / n;
+                int newCol = newIndex % n;
+
+                ans[newRow][newCol] = grid[i][j];
             }
-
-            matrix.add(row);
         }
-        return matrix;
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                row.add(ans[i][j]);
+            }
+            result.add(row);
+        }
+
+        return result;
     }
 }
