ff43ea57a0371a62e929553ac12696f4f89b6925
RUDRANSH <rudranshupadhyay2004@gmail.com>
2026-08-14 13:41:12 +0530

    Time: 10 ms (9.13%), Space: 46.5 MB (8.85%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,26 +1,19 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        int max =0;
-        for(int i = 0; i<s.length();i++){
-            Map<Character,Integer> mpp = new HashMap<>();
-            // mpp.put(s.charAt(i),1);
-            int j=i;
-            while(j<s.length() && i<=j ){
-                if(!mpp.containsKey(s.charAt(j))){
-                    mpp.put(s.charAt(j),1);
-                    j++;
-                }
-                else if(mpp.containsKey(s.charAt(j))&&mpp.get(s.charAt(j))<2){
-                    mpp.put(s.charAt(j),mpp.get(s.charAt(j))+1);
-                    max = Math.max(max,j-i+1);
-                    j++;
-                }else{
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            Map<Character, Integer> mpp = new HashMap<>();
+            for (int j = i; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                if (mpp.getOrDefault(ch, 0) == 2) {
                     break;
                 }
-                max = Math.max(max,j-i);
+                mpp.put(ch, mpp.getOrDefault(ch, 0) + 1);
+                max = Math.max(max, j - i + 1);
             }
         }
+
         return max;
-    
     }
 }
